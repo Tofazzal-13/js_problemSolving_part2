9a23166ba2c63b2e66ca28f4db5cc481a42cd5b6
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-08 22:59:04 +0600

    object declare

---
+++ array.js
@@ -0,0 +1,3 @@
+const products = [
+    {name: "phone", price: 250000}
+]
---
+++ object.js
@@ -0,0 +1,27 @@
+const nayok = {
+    name: "Sakib Khan",
+    id: 121, 
+    address: "movie cinema",
+    isSingle: true, 
+    friends: ["apu", "Raaz", "Salman", "amir"],
+    movies: [{name: "no-1", year: 2015},{name: "king Khan", year: 2018}],
+    act: function(){
+        console.log("actine like sakib khan");
+        
+    },
+    car:  {
+        brand: "tesla",
+        price: 50000000,
+        made: 2025,
+        manufacturer: {
+            name : "tesla",
+            ceo: "Elon Mask",
+            country: "USA"
+        }
+    }
+}
+
+
+console.log(nayok.act);
+nayok.act()
+
